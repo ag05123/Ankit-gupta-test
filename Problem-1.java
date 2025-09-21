d9f0525e6d0ee29d3e393fe2b58711cf91da0a75
Ankit Gupta <132796891+ag05123@users.noreply.github.com>
2025-09-21 14:55:59 +0530

    Update Problem-1.java

--- Problem-1.java
+++ Problem-1.java
@@ -1,1 +1,26 @@
 
+class Calculator {
+    double a, b;
+    String operation;
+
+    Calculator(double a, double b, String operation) {
+        this.a = a;
+        this.b = b;
+        this.operation = operation.toLowerCase();
+    }
+
+    public double calculate() {
+        switch (operation) {
+            case "add": return a + b;
+            case "subtract": return a - b;
+            case "multiply": return a * b;
+            case "divide": return (b != 0) ? a / b : Double.NaN;
+            default: return Double.NaN;
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator(10, 5, "divide");
+        System.out.println("Result: " + calc.calculate());
+    }
+}
