d993903889778b49175d6f424b784314a3f7f986
Ankit Gupta <132796891+ag05123@users.noreply.github.com>
2025-09-21 14:57:05 +0530

    Update Problem-3.java

--- Problem-3.java
+++ Problem-3.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        int length = (a % 2 == 0) ? a - 1 : a;
+        for (int i = 1; i <= length * 2; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
